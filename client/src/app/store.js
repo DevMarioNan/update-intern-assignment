818f788fa2153d501804705d09316f20cc1b3f1c
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2024-02-17 17:01:41 +0200

    Setting up redux store

---
+++ client/src/app/store.js
@@ -0,0 +1,32 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import {
+    
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+const persistedReducer = persistReducer(persistConfig);
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
+});
+
