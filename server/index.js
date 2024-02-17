6aac53e3b545cca7fd556861db91b3aefa5aedac
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2024-02-17 15:34:37 +0200

    Connection to database

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`);
+mongoose.connect(process.env.DB_URL).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log(err);
 });
