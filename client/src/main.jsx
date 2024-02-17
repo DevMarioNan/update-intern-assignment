90f88e3803f71f75c5111a58ebc52633cedb0839
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2024-02-17 17:03:16 +0200

    Connecting the store to the application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from "./app/store"
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+      
+    </Provider>
   </React.StrictMode>,
 )
